b6bb3dfe9e2b7e34f42f1fd8b8251668d6b795ef
rjgehan <rjgehan@loyola.edu>
2024-02-27 02:03:54 -0500

    renoved api

--- API/DestinyCalculator.java
+++
@@ -1,9 +0,0 @@
-class DestinyCalculator {
-    public static void main(String[] args) {
-        print("hello world");
-    }
-
-    public static void print(String out) {
-        System.out.println(out);
-    }
-}
