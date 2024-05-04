a09c617b8e4912d8ffcea2065d22b49e16d38bfe
rjgehan <rjgehan@loyola.edu>
2024-05-04 05:37:08 -0400

    Update pageFunctions.js

--- DTB/pageFunctions.js
+++ DTB/pageFunctions.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   fetch("gunstyles.json")
   .then(response => {
       if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error('Network response was not OK! ' + response.statusText);
       }
       return response.json();
   })
